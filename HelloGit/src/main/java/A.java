4e8dc1210fbaa0dc79d2c87fa55e8dffe528af8a
shuihongqing <shuihongqing@163.com>
2020-11-10 12:39:35 +0800

    master

--- HelloGit/src/main/java/A.java
+++ HelloGit/src/main/java/A.java
@@ -1,7 +1,8 @@
 public class A {
 
     public static void main(String[] args) {
-        System.out.println("创建分支dev");
+        System.out.println("创建分支dev1");
+        System.out.println("master");
     }
 
 } 
